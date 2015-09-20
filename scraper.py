2ebeab2af313f473aeb5360ffe1ffa69789f1bb5
Charles Shinaver <bshinaver@gmail.com>
2015-09-20 16:16:38 -0400

    Added initial attempt at getting books from amazon search results

--- scraper.py
+++ scraper.py
@@ -25,6 +25,21 @@ class Book:
             )
         )
 
+    def __repr__(self):
+        return (
+            "Title: {title}\n"
+            "Price: {price}\n"
+            "rental: {rental}\n"
+            "book type: {book_type}\n"
+            "link: {link}\n".format(
+                title=self.title,
+                price=self.price,
+                rental=self.is_rental,
+                book_type=self.book_type,
+                link=self.link,
+            )
+        )
+
 
 def get_page_for_amazon_book_search(keyword):
     search_string = (
@@ -37,14 +52,62 @@ def get_page_for_amazon_book_search(keyword):
     return requests.get(search_string).content
 
 
-def get_amazon_book_prices_for_keyword(keyword):
-    content = get_page_for_amazon_book_search(keyword)
-    soup = BeautifulSoup(content)
-    list_items = soup.find_all('li', class_='s-result-item celwidget')
-    for item in list_items:
-        print item.find_all(
+def get_amazon_books_for_keyword(keyword):
+    def get_book_list_items_from_content(content):
+        """ Returns Amazon book items in BeautifulSoup format """
+        soup = BeautifulSoup(content, 'html.parser')
+        book_list_items = soup.find_all('li', class_='s-result-item celwidget')
+        return book_list_items
+
+    def parse_price_bulk_item_into_book(item, title):
+        new_book = Book()
+        price_tag = item.find(
             'span',
-            class_='a-size-base a-color-price s-price a-text-bold',
+            class_="a-size-base a-color-price s-price a-text-bold",
         )
 
-get_amazon_book_prices_for_keyword('compilers')
+        # if does not contain price, ignore
+        if not price_tag:
+            return
+
+        new_book.price = price_tag.get_text()
+        new_book.link = item.attrs['href']
+        new_book.title = title
+
+        # determine if is rental
+        rent_or_buy_item = item.find(
+            'span',
+            class_='a-color-secondary',
+        )
+        if rent_or_buy_item:
+            rent_or_buy_text = rent_or_buy_item.get_text()
+            new_book.is_rental = "rent" in rent_or_buy_text
+
+        return new_book
+
+    def parse_book_list_item_into_books(item):
+        # Title and link contained in link element
+        link_item = item.find(
+            'a',
+            class_="a-link-normal s-access-detail-page a-text-normal",
+        )
+        new_book_title = link_item.attrs['title']
+
+        # Handle pricing for hardcover, Paperback, Kindle Edition, rent
+        price_column_item = item.find('div', class_="a-column a-span7")
+        price_bulk_items = price_column_item.find_all(
+            'a',
+            class_='a-link-normal a-text-normal',
+        )
+
+        new_books = []
+        for item in price_bulk_items:
+            book = parse_price_bulk_item_into_book(item, new_book_title)
+            if book:
+                new_books.append(book)
+        return new_books
+
+    content = get_page_for_amazon_book_search(keyword)
+    items = get_book_list_items_from_content(content)
+    book_lists = map(parse_book_list_item_into_books, items)
+    return [item for sublist in book_lists for item in sublist]
