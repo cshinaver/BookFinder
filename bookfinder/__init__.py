4b3100ee94df68100b4bfe495c3c333185fb13cd
Charles Shinaver <bshinaver@gmail.com>
2015-10-26 22:15:15 -0400

    Created api and search modules

--- bookfinder/__init__.py
+++ bookfinder/__init__.py
@@ -3,6 +3,6 @@ from flask import Flask
 app = Flask(__name__)
 
 import bookfinder.views  # noqa
-import bookfinder.search  # noqa
+import bookfinder.search.views  # noqa
 import bookfinder.prices  # noqa
-import bookfinder.api  # noqa
+import bookfinder.api.views  # noqa
