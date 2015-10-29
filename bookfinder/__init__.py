0ca15b0a1bdd54764a6a2157b013772145566294
Charles Shinaver <bshinaver@gmail.com>
2015-10-28 21:01:20 -0400

    load development settings by default

--- bookfinder/__init__.py
+++ bookfinder/__init__.py
@@ -6,11 +6,14 @@ app = Flask(__name__)
 
 # Config
 # BOOKFINDER_SETTINGS must be set to development or production
-settings_type = os.environ['BOOKFINDER_SETTINGS']
+settings_type = os.environ.get('BOOKFINDER_SETTINGS', None)
 
 if settings_type == "development":
     app.config.from_object('bookfinder.settings.DevelopmentConfig')
 elif settings_type == "production":
     app.config.from_object('bookfinder.settings.ProductionConfig')
+elif not settings_type:
+    app.config.from_object('bookfinder.settings.DevelopmentConfig')
+
 
 import bookfinder.views  # noqa
