d1b93f1b48cf73100350c7fd2b9a813473dc62b6
Charles Shinaver <bshinaver@gmail.com>
2015-11-01 17:47:24 -0500

    Added load_user method for flask-login

--- bookfinder/models/bookfinderuser.py
+++ bookfinder/models/bookfinderuser.py
@@ -1,8 +1,16 @@
+from flask.ext.login import UserMixin
+
+from bookfinder import login_manager
 from bookfinder.models.base import BaseModel
 
 
-class BookfinderUser(BaseModel):
+class BookfinderUser(BaseModel, UserMixin):
     id = None
     username = None
     email = None
     password = None
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return BookfinderUser.get(user_id)
