b293fa2a1f192ad79fb71764589fd5aae1c5d2eb
mperez14 <mperez14@nd.edu>
2015-11-02 10:27:27 -0500

    stash

--- bookfinder/purchase/form.py
+++ bookfinder/purchase/form.py
@@ -1,7 +1,9 @@
 from flask.ext.wtf import Form
-from wtforms import StringField, BooleanField
+from wtforms import StringField, BooleanField, DecimalField
 from wtforms.validators import DataRequired
 
 class LoginForm(Form):
     ISBN = StringField('ISBN', validators=[DataRequired()])
-    Price = StringField('Price', validators=[DataRequired()])
+    Price = DecimalField('Price', validators=[DataRequired()])
+    Title = StringField('Title', validators=[DataRequired()])
+    Author = StringField('Author', validators = [DataRequired()])
--- bookfinder/purchase/views.py
+++ bookfinder/purchase/views.py
@@ -3,6 +3,8 @@ from bookfinder import app
 import scraper
 from scraper import get_book_object_for_book_title
 from .form import LoginForm
+from bookfinder.models.book import Book
+from bookfinder.models.purchasechoice import PurchaseChoice
 
 @app.route('/purchase/', methods = ['GET', 'POST'])
 def home():
@@ -22,12 +24,29 @@ def home():
 			ISBN = request.form['ISBN']
 			Price = request.form['Price']
 			Type = 'Book'
-			isRental = False;
+			isRental = False
+			Author = request.form['Author']
+			Title = request.form['Title']
 
 			#check if book has correct ISBN
 			#import ipdb;ipdb.set_trace()
 			if(get_book_object_for_book_title(ISBN) != -1):
 				#add book to DB
+				b = Book()
+				b.author = Author
+				b.title = Title
+				b.isbn = ISBN
+				b.save()
+
+				#add purchaseChoice to DB
+				p = PurchaseChoice()
+				p.book_id = b.id
+				p.price = Price
+				type = Hardcover
+				isRental = False
+				link = ''		#change to valid link
+				seller = 'Foo Bar Seller' #change to user once login is created
+				p.save()
 				return redirect('')
 			else:	# book is not valid
 				error('ISBN is no good')
