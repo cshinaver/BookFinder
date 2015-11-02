8402c08d783d2fff55a134b6fff6ae6be5795e84
cshinaver <bshinaver@gmail.com>
2015-11-01 20:13:16 -0500

    User passwords now hashed and salted

--- bookfinder/models/bookfinderuser.py
+++ bookfinder/models/bookfinderuser.py
@@ -8,7 +8,18 @@ class BookfinderUser(BaseModel, UserMixin):
     id = None
     username = None
     email = None
-    password = None
+    pw_hash = None
+
+    def __init__(self, username=None, password=None):
+        self.username = username
+        if password:
+            self.set_password(password)
+
+    def set_password(self, password):
+        self.pw_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.pw_hash, password)
 
 
 @login_manager.user_loader
