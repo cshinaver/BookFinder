abfd4f43e6865b1dfa77d858b54e1b5a743d6b21
Charles Shinaver <bshinaver@gmail.com>
2015-11-11 15:07:17 -0500

    issue #61 Converted SellBook page to class based view for better ordering

--- bookfinder/purchase/views.py
+++ bookfinder/purchase/views.py
@@ -1,4 +1,5 @@
 from flask import render_template, request, flash, redirect
+from flask.views import View
 from bookfinder import app
 from bookfinder.api.scraper import get_book_object_list_for_book_title
 from bookfinder.purchase.form import SellBookForm
@@ -7,66 +8,70 @@ from bookfinder.models.purchasechoice import PurchaseChoice
 from flask.ext.login import current_user, login_required
 
 
-@app.route('/purchase/', methods=['GET', 'POST'])
-@login_required
-def home():
-    form = SellBookForm()
-    if request.method == 'POST':
-        ISBN = request.form['ISBN']
-        Price = request.form['Price']
+class SellBook(View):
+    methods = ['GET', 'POST']
 
-        if ISBN == '':
-            flash(u'No ISBN entered', 'error')
-        if Price == '':
-            flash(u"No Price entered", "error")
-        len_isbn = len(ISBN)
-        if len_isbn != 10 and len_isbn != 13:
-            flash(
-                "ISBN has incorrect number of characters. "
-                "Please enter ISBN-10 or ISBN-13",
-                'error',
-            )
-        qry_var = get_book_object_list_for_book_title('isbn:'+ISBN)
-        if not qry_var:
-            flash('ISBN is invalid', 'error')
-        elif Price != '' and ISBN != '' and (len_isbn == 10 or len_isbn == 13):
-            # Check if book has correct ISBN
-            # Check DB for duplicate isbn entry.
-            # If so just make purchase option
-            temp_book = qry_var[0]
-            Author = temp_book.author
-            ISBN = temp_book.isbn
-            if(temp_book.subtitle is None):
-                Title = temp_book.title
-            else:
-                Title = temp_book.title+': '+temp_book.subtitle
+    def dispatch_request(self):
+        form = SellBookForm()
+        if request.method == 'POST':
+            ISBN = request.form['ISBN']
+            Price = request.form['Price']
 
-            #  add book to DB
-            b = Book()
-            b.author = Author
-            b.title = Title
-            b.isbn = ISBN
+            # Error checking
+            if ISBN == '':
+                flash(u'No ISBN entered', 'error')
+            if Price == '':
+                flash(u"No Price entered", "error")
+            len_isbn = len(ISBN)
+            if len_isbn != 10 and len_isbn != 13:
+                flash(
+                    "ISBN has incorrect number of characters. "
+                    "Please enter ISBN-10 or ISBN-13",
+                    'error',
+                )
+            qry_var = get_book_object_list_for_book_title('isbn:'+ISBN)
+            if not qry_var:
+                flash('ISBN is invalid', 'error')
+            elif Price != '' and ISBN != '' and (len_isbn == 10 or len_isbn == 13):
+                # Check if book has correct ISBN
+                # Check DB for duplicate isbn entry.
+                # If so just make purchase option
+                temp_book = qry_var[0]
+                Author = temp_book.author
+                ISBN = temp_book.isbn
+                if(temp_book.subtitle is None):
+                    Title = temp_book.title
+                else:
+                    Title = temp_book.title+': '+temp_book.subtitle
 
-            temp_book_item = Book.get(isbn=ISBN)
-            if not (temp_book_item):  # if get returns true, already book in DB
-                b.save()
-            else:
-                b.id = temp_book_item.id
+                #  add book to DB
+                b = Book()
+                b.author = Author
+                b.title = Title
+                b.isbn = ISBN
 
-            # add purchaseChoice to DB
-            p = PurchaseChoice()
-            p.book_id = b.id
-            p.price = Price
-            p.type = 'Hardcover'
-            p.isRental = False
-            p.link = ''  # change to valid link
-            # change to user once login is created
-            p.local_seller_id = current_user.id
-            p.isLocalSeller = True
-            p.save()
-            flash('Your book has been logged into the database!', 'success')
-            return redirect('')
-    return render_template(  # reference html script
-        'purchase/purchase_index.html',
-        form=form,
-    )
+                temp_book_item = Book.get(isbn=ISBN)
+                if not (temp_book_item):  # if get returns true, already book in DB
+                    b.save()
+                else:
+                    b.id = temp_book_item.id
+
+                # add purchaseChoice to DB
+                p = PurchaseChoice()
+                p.book_id = b.id
+                p.price = Price
+                p.type = 'Hardcover'
+                p.isRental = False
+                p.link = ''  # change to valid link
+                # change to user once login is created
+                p.local_seller_id = current_user.id
+                p.isLocalSeller = True
+                p.save()
+                flash('Your book has been logged into the database!', 'success')
+                return redirect('')
+        return render_template(  # reference html script
+            'purchase/purchase_index.html',
+            form=form,
+        )
+
+app.add_url_rule('/purchase/', view_func=SellBook.as_view('sell_book'))
