71ac7e8b7259dd511b3089a0054a504df1f06f54
Charles Shinaver <bshinaver@gmail.com>
2015-11-28 23:49:28 -0500

    issue #68 Added initial bookrecommender as separate project

---
+++ bookrecommender/README.md
@@ -0,0 +1,35 @@
+
+# bookrecommender
+
+A barebones Clojure app, which can easily be deployed to Heroku.  
+
+This application support the [Getting Started with Clojure](https://devcenter.heroku.com/articles/getting-started-with-clojure) article - check it out.
+
+## Running Locally
+
+Make sure you have Clojure installed.  Also, install the [Heroku Toolbelt](https://toolbelt.heroku.com/).
+
+```sh
+$ git clone https://github.com/heroku/bookrecommender.git
+$ cd bookrecommender
+$ lein repl
+user=> (require 'bookrecommender.web)
+user=>(def server (bookrecommender.web/-main))
+```
+
+Your app should now be running on [localhost:5000](http://localhost:5000/).
+
+## Deploying to Heroku
+
+```sh
+$ heroku create
+$ git push heroku master
+$ heroku open
+```
+
+## Documentation
+
+For more information about using Clojure on Heroku, see these Dev Center articles:
+
+- [Clojure on Heroku](https://devcenter.heroku.com/categories/clojure)
+
---
+++ bookrecommender/src/bookrecommender/mahout.clj
@@ -0,0 +1,22 @@
+(ns bookrecommender.mahout
+  (:import
+   [org.apache.mahout.cf.taste.impl.model.file FileDataModel]
+   [org.apache.mahout.cf.taste.impl.neighborhood ThresholdUserNeighborhood]
+   [org.apache.mahout.cf.taste.impl.recommender GenericBooleanPrefUserBasedRecommender]
+   [org.apache.mahout.cf.taste.impl.similarity TanimotoCoefficientSimilarity]
+   [org.apache.mahout.cf.taste.neighborhood UserNeighborhood]
+   [org.apache.mahout.cf.taste.recommender Recommender]
+   [org.apache.mahout.cf.taste.similarity UserSimilarity]
+   [java.io File]))
+
+(defn recommend [filepath user-id num-recommendations]
+  (let [model (FileDataModel. (File. filepath))
+        similarity (TanimotoCoefficientSimilarity. model)
+        neighborhood (ThresholdUserNeighborhood. 0.1, similarity, model)
+        recommender (GenericBooleanPrefUserBasedRecommender. model, neighborhood, similarity)]
+    (.recommend recommender user-id num-recommendations)
+))
+
+(map
+  #(hash-map :item-id (.getItemID %) :value (.getValue %))
+  (recommend "book.csv" 2 3))
