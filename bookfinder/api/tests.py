f550a64b2372889942942801a800b734142c9bc3
Charles Shinaver <bshinaver@gmail.com>
2015-12-13 16:55:16 -0500

    issue #89 Added vanilla test for amazon scraper

---
+++ bookfinder/api/tests.py
@@ -0,0 +1,23 @@
+import unittest
+
+from bookfinder.api.scraper import AmazonScraper
+
+
+class ApiScraperTestCase(unittest.TestCase):
+    def test_amazon_scraper_gets_purchase_choices(self):
+        """
+        PurchaseChoice should have isbn, title, price, seller, thumbnail_link,
+        book_type, regular_link, and rental
+        """
+        book_title = "Compilers"
+        scraper = AmazonScraper()
+        ls = scraper.get_amazon_purchase_choices_for_keyword(book_title)
+        b = ls[0]
+        self.assertNotEqual(b['isbn'], '')
+        self.assertNotEqual(b['title'], '')
+        self.assertNotEqual(b['price'], '')
+        self.assertNotEqual(b['seller'], '')
+        self.assertNotEqual(b['thumbnail_link'], '')
+        self.assertNotEqual(b['book_type'], '')
+        self.assertNotEqual(b['link'], '')
+        self.assertNotEqual(b['rental'], '')
