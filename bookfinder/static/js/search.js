a8ebcfbb7c97b8e1ca3b0aca9954590578e63c9d
Charles Shinaver <bshinaver@gmail.com>
2015-12-16 17:35:37 -0500

    issue #91 Added retry to js getting book results

--- bookfinder/static/js/search.js
+++ bookfinder/static/js/search.js
@@ -1,16 +1,24 @@
 function get_book_list(query) {
-    get_file('/api/books_list/?title=' + query, function(text) {
-        document.getElementById("result-list").innerHTML = "";
-        book_list = JSON.parse(text);
-        var num_books = book_list.length;
-        if(num_books) {
-            for(var i = 0; i < num_books; i++) {
-                add_book_to_list(book_list[i]);
+    function get_book_list_helper(retries) {
+        var max_retries = 5;
+        get_file('/api/books_list/?title=' + query, function(text) {
+            document.getElementById("result-list").innerHTML = "";
+            book_list = JSON.parse(text);
+            var num_books = book_list.length;
+            if(num_books) {
+                for(var i = 0; i < num_books; i++) {
+                    add_book_to_list(book_list[i]);
+                }
             }
-        } else { // doesn't seem to be working
-            document.getElementById("result-list").innerHTML = "Sorry. There are no Book results for the search query \"" + query + "\".";
-        }
-    });
+            else if (retries < max_retries) {
+                retries += 1;
+                get_book_list_helper(retries);
+            } else { // doesn't seem to be working
+                document.getElementById("result-list").innerHTML = "Sorry. There are no Book results for the search query \"" + query + "\".";
+            }
+        });
+    }
+    get_book_list_helper(0);
 }
 
 function fill_search(text) {
